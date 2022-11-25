b06489c220ae9138b0936965ecbeb24c4e467f6b
godoymarcos27 <duabua1@gmail.com>
2022-11-24 22:35:55 -0600

    tarea-7.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pantalla1Component } from './pantalla1/pantalla1.component';
+import { Pantalla2Component } from './pantalla2/pantalla2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pantalla1', component: Pantalla1Component },
+  { path: 'pantalla2', component: Pantalla2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pantalla1Component } from './pantalla1/pantalla1.component';
+import { Pantalla2Component } from './pantalla2/pantalla2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pantalla1Component,
+    Pantalla2Component
   ],
   imports: [
     BrowserModule,
